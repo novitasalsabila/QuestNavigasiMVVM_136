2c36e0501e0f37eed03d7c069a3ca456b87da0bc
novitasalsabila <novnovv10@gmail.com>
2024-11-21 18:48:46 +0700

    Kode ini menampilkan key-value dalam satu baris, menggunakan Row dengan proporsi ruang yang diatur oleh Modifier.weight untuk tata letak yang rapi.

--- app/src/main/java/com/example/arsitekturmvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/arsitekturmvvm/viewmodel/SiswaViewModel.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-
 class SiswaViewModel: ViewModel() {
     //Request
     private val _statusUI = MutableStateFlow(DataSiswa())
